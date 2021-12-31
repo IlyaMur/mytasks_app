add339cb2d88fa54f7555548f1c4c4f8d1995063
Ilya Mur <ilyamur@hotmail.com>
2022-01-01 02:22:35 +0300

    add dotenv

--- api/index.php
+++ api/index.php
@@ -2,13 +2,26 @@
 
 declare(strict_types=1);
 
+use Dotenv\Dotenv;
+use TasksApp\Database;
+use TasksApp\TaskController;
+
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 set_exception_handler('TasksApp\ErrorHandler::handleException');
 
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->safeload();
 
-$parts = explode('/', $path);
+$dbName = $_ENV['DB_NAME'] ?? getenv('DB_HOST');
+$dbUser = $_ENV['DB_USER'] ?? getenv('DB_USER');
+$dbPass = $_ENV['DB_PASS'] ?? getenv('DB_PASS');
+$dbHost = $_ENV['DB_HOST'] ?? getenv('DB_HOST');
+
+$parts = explode(
+    '/',
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
 
 $resource = $parts[2];
 $id = $parts[3] ?? null;
@@ -19,6 +32,9 @@ if ($resource !== 'tasks') {
 }
 
 header('Content-type: application/json; charset=UTF-8');
-$controller = new TasksApp\TaskController();
 
+$database = new Database($dbHost, $dbName, $dbUser, $dbPass);
+$database->getConnection();
+
+$controller = new TaskController();
 $controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
