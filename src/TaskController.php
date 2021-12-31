7d8706807446fe73fec518ae8f924bcaef67a9f3
Ilya Mur <ilyamur@hotmail.com>
2021-12-31 03:12:31 +0300

    add respondMethodNotAllowed method to respond allowed http methods for api

--- src/TaskController.php
+++ src/TaskController.php
@@ -13,6 +13,8 @@ class TaskController
                 echo 'index';
             } elseif ($method === 'POST') {
                 echo 'create';
+            } else {
+                $this->respondMethodNotAllowed('GET, POST');
             }
         } else {
             switch ($method) {
@@ -25,7 +27,15 @@ class TaskController
                 case "DELETE":
                     echo "delete $id";
                     break;
+                default:
+                    $this->respondMethodNotAllowed('GET, PATCH, DELETE');
             }
         }
+    }
+
+    private function respondMethodNotAllowed(string $allowedMethods): void
+    {
+        http_response_code(405);
+        header("Allow: $allowedMethods");
     }
 }
