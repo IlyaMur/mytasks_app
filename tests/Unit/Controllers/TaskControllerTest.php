a702edb9c6c8058281509bcdf910876a6b382285
Ilya Mur <ilyamur@hotmail.com>
2022-01-21 14:15:54 +0300

    one more case fix

--- tests/Unit/Controllers/TaskControllerTest.php
+++ tests/Unit/Controllers/TaskControllerTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Ilyamur\TaskApp\Tests\Unit\Controllers;
 
 use Ilyamur\TaskApp\Tests\Unit\Controllers\TestDoubles\TaskControllerChild;
-use Ilyamur\TasksApp\Controllers\TaskController;
 use Ilyamur\TasksApp\Gateways\TaskGateway;
 use PHPUnit\Framework\TestCase;
 
--- tests/Unit/Controllers/TokenControllerTest.php
+++ tests/Unit/Controllers/TokenControllerTest.php
@@ -7,7 +7,6 @@ namespace Ilyamur\TaskApp\Tests\Unit\Controllers;
 use PHPUnit\Framework\TestCase;
 use Ilyamur\TasksApp\Services\JWTCodec;
 use Ilyamur\TasksApp\Gateways\UserGateway;
-use Ilyamur\TasksApp\Gateways\refreshTokenGateway;
 use Ilyamur\TasksApp\Gateways\RefreshTokenGateway;
 use Ilyamur\TaskApp\Tests\Unit\Controllers\TestDoubles\TokenControllerChild;
 
