955f5a1489b670c3d972642260f8b00c663f94da
Ilya Mur <ilyamur@hotmail.com>
2022-01-29 18:08:50 +0300

    change sql to non auto increment

--- database/mytasks.sql
+++ database/mytasks.sql
@@ -1,36 +1,18 @@
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.11
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `mytasks`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `refresh_token`
---
 
 CREATE TABLE `refresh_token` (
   `token_hash` varchar(256) NOT NULL,
   `expires_at` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `task`
---
 
 CREATE TABLE `task` (
   `id` int(11) NOT NULL,
@@ -41,12 +23,6 @@ CREATE TABLE `task` (
   `completed` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `email` varchar(128) DEFAULT NULL,
@@ -55,56 +31,28 @@ CREATE TABLE `user` (
   `api_key` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `refresh_token`
---
 ALTER TABLE `refresh_token`
   ADD PRIMARY KEY (`token_hash`),
   ADD KEY `expires_at` (`expires_at`);
 
---
--- Indexes for table `task`
---
 ALTER TABLE `task`
   ADD PRIMARY KEY (`id`),
   ADD KEY `name` (`title`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Indexes for table `user`
---
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `api_key` (`api_key`),
   ADD UNIQUE KEY `email` (`email`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `task`
---
 ALTER TABLE `task`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=185;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `user`
---
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=151;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `task`
---
 ALTER TABLE `task`
   ADD CONSTRAINT `task_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
